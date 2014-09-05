60de6ac0dc7e77da311448a152a60e1983edbb74
byverdu <byverdu@gmail.com>
2014-09-05 11:28:31 +0100

    added test for takeaway menu

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,10 +1,19 @@
+require "dish"
+
 class Takeaway
 
-	attr_accessor :name, :number
+	attr_accessor :name, :number, :menu
 
 	def initialize(name=nil, number=nil)
-		@name   = name
-		@number = number
+		@name    = name
+		@number  = number
+		@menu    =  create_menu
+	end
+
+	def create_menu
+		dishes = [Dish.new('scampi',8.5),Dish.new('coleslaw',2.5),Dish.new('burger',4.5),Dish.new('coke',1.5),Dish.new('fish cake',5.5)]
+
+		dishes
 	end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,6 +4,9 @@ describe Takeaway do
 
 	let(:takeaway) { Takeaway.new('Casa Ramiro', '07730879056') }
 
+	let(:dish) { double :dish, name: 'scampi' }
+
+
 	it "has his own name" do
 		expect(takeaway.name).to eq('Casa Ramiro')
 	end
@@ -11,5 +14,9 @@ describe Takeaway do
 	it "has a telephone number" do
 		expect(takeaway.number).to eq('07730879056')
 	end
-  
+
+	it "has a menu with several dishes" do
+		expect(takeaway.menu).to be_a(Array)
+		expect(takeaway.menu.first.name).to eq(dish.name)	
+	end
 end
